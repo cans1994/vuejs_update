7c701a1d5e433f6c5671c9a554cd82d45d0c5eca
Cansu Bal <95681316+cans1994@users.noreply.github.com>
2022-02-18 16:13:28 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <Menu/>
+    <button v-on:click="say('Accueil')">Accueil</button>
+    <button v-on:click="say('Apropos')">À propos</button>
+    <button v-on:click="say('MentionsLegales')">Mentions légales</button>
+
+    <Accueil/>
+    <Apropos/>
+    <MentionsLegales/>
+
+    
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+import Accueil from './components/Accueil.vue'
+import Apropos from './components/Apropos.vue'
+import MentionsLegales from './components/MentionsLegales.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Menu,
+    Accueil,
+    Apropos,
+    MentionsLegales,
+
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Staatliches', cursive;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
